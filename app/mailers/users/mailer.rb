1a6ec3ad7f564414250ed361a049c74a0777b99c
EC2 Default User <ec2-user@ip-172-31-33-216.ec2.internal>
2021-02-05 11:19:49 +0000

    test new file

--- app/mailers/users/mailer.rb
+++ app/mailers/users/mailer.rb
@@ -2,6 +2,8 @@ class Users::Mailer < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
+  default from: "noreply@realtimesocial.jp"
+  
   def confirmation_instructions(record, token, opts={})
     opts[:subject] = "【Realtime Social】メールアドレスの確認"
     super
